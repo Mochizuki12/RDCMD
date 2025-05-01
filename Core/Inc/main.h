bad801f05e76cb65e78a120a0854b3a67c7b1f97
Mochizuki12 <yuyakemochizuki12@gmail.com>
2025-05-01 17:38:06 +0900

    v0.2.0

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -49,8 +49,6 @@ extern "C" {
 
 /* USER CODE END EM */
 
-void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
-
 /* Exported functions prototypes ---------------------------------------------*/
 void Error_Handler(void);
 
@@ -59,22 +57,24 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define LED1_Pin GPIO_PIN_4
-#define LED1_GPIO_Port GPIOC
-#define LED2_Pin GPIO_PIN_5
-#define LED2_GPIO_Port GPIOC
-#define EN2_Pin GPIO_PIN_2
-#define EN2_GPIO_Port GPIOB
-#define CANID4_Pin GPIO_PIN_12
-#define CANID4_GPIO_Port GPIOB
-#define CANID3_Pin GPIO_PIN_13
-#define CANID3_GPIO_Port GPIOB
-#define CANID2_Pin GPIO_PIN_14
-#define CANID2_GPIO_Port GPIOB
-#define CANID1_Pin GPIO_PIN_15
-#define CANID1_GPIO_Port GPIOB
-#define EN1_Pin GPIO_PIN_8
-#define EN1_GPIO_Port GPIOA
+#define CANID1_Pin GPIO_PIN_0
+#define CANID1_GPIO_Port GPIOC
+#define CANID2_Pin GPIO_PIN_1
+#define CANID2_GPIO_Port GPIOC
+#define CANID3_Pin GPIO_PIN_2
+#define CANID3_GPIO_Port GPIOC
+#define CANID4_Pin GPIO_PIN_3
+#define CANID4_GPIO_Port GPIOC
+#define SD_detector_Pin GPIO_PIN_4
+#define SD_detector_GPIO_Port GPIOA
+#define SW1_Pin GPIO_PIN_5
+#define SW1_GPIO_Port GPIOA
+#define EN2_Pin GPIO_PIN_4
+#define EN2_GPIO_Port GPIOC
+#define EN1_Pin GPIO_PIN_5
+#define EN1_GPIO_Port GPIOC
+#define cycle_Pin GPIO_PIN_12
+#define cycle_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
--- Debug/Core/Src/main.d
+++ Debug/Core/Src/main.d
@@ -25,8 +25,12 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_sd.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_sdmmc.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ ../Core/Inc/can.h ../Core/Inc/main.h ../Core/Inc/dma.h \
+ ../Core/Inc/sdio.h ../Core/Inc/tim.h ../Core/Inc/gpio.h
 ../Core/Inc/main.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
 ../Core/Inc/stm32f4xx_hal_conf.h:
@@ -54,5 +58,13 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_sd.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_sdmmc.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+../Core/Inc/can.h:
+../Core/Inc/main.h:
+../Core/Inc/dma.h:
+../Core/Inc/sdio.h:
+../Core/Inc/tim.h:
+../Core/Inc/gpio.h:
